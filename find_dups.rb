e6a7cddc7c66c6fb9d2aaba30bbe93450d0d4880
Bartłomiej Kalisz <barfek@gmail.com>
2012-04-01 15:48:49 +0200

    Few changes and compare by md5 added

--- find_dups.rb
+++ find_dups.rb
@@ -1,36 +1,77 @@
-# -*- coding: utf-8 -*-
-#Script to find duplicates of files
+#class for searching duplicates of files
 
-folders = Dir['**/']
-files = Array.new()
-files_nodups = Array.new()
-files_dups = Array.new()
+require 'digest/md5'
 
-folders.each do |i|
-  Dir.chdir(i) do
-    files += Dir['*.*']
-    files_nodups |= Dir['*.*']
+class Duplicates
+
+  attr_reader :files, :duplicates
+
+  def initialize(start_dir)
+    @start_dir = start_dir
+    @files = Array.new()
+    @md5 = Hash.new()
+    @duplicates = Hash.new()
   end
-end
 
-temp = Array.new(files)
+  # list duplicates
 
-until temp==[]
-  last = temp.pop
-  if temp.include?(last)
-    temp.delete(last)
-    files_dups << last
+  def list_dups(include_subfolders = false)
+    @include_subfolders = include_subfolders
+    load_files
+    calculate_md5
+    search_for_duplicates
+    if @duplicates == {}
+      puts "There are no duplicates!"
+    else
+      @duplicates.each do |d|
+        puts d[1].to_s + " => " + d[0].to_s
+      end
+    end
+    return nil
   end
-end
+  
+  # list all files & their md5s
+  
+  def list_all(include_subfolders = false)
+    @include_subfolders = include_subfolders
+    load_files
+    calculate_md5
+    @files.each do |f|
+      puts f + "=" + @md5[f]
+    end
+    return nil
+  end
 
-puts "Total nr of files: #{files.size}"
-puts "Number of individual files: #{files_nodups.size}"
-puts "Nr of files with duplicate names: #{files_dups.size}"
+  private
 
-puts "View duplicates? (y/n): "
-ans = gets.chomp()
+  # load list of files
 
-if ans == "y"
-  puts files_dups
-  puts "> #{files_dups.size} duplicates total."
-end
+  def load_files
+    @files =
+    Dir.chdir(@start_dir) do
+      if @include_subfolders == false
+        Dir['*.*']
+      else
+        Dir['**/*.*']
+      end
+    end
+  end
+
+  # calculate md5 for each file
+
+  def calculate_md5
+    @files.each do |f|
+      @md5[f] = Digest::MD5.hexdigest(File.read(@start_dir + "/" + f))
+    end
+  end
+
+  # search for duplicates
+
+  def search_for_duplicates
+    d_md5 = Hash[@md5.sort - @md5.invert.invert.sort].invert.keys.sort
+    @duplicates = @md5.dup
+    @duplicates.delete_if {|k,v| not(d_md5.include?(v))}
+    @duplicates = Hash[@duplicates.sort_by {|k,v| v}]
+  end
+
+ end
